8e8878f83ae320b26d80a416f91792bbe0be8298
bolicos <apollobolico@gmail.com>
2024-08-14 19:10:46 -0300

    enhancement: add dragon domain feature scratch

--- src/main/java/br/bolicos/api/dragons/application/repositories/DragonRepository.java
+++ src/main/java/br/bolicos/api/dragons/application/repositories/DragonRepository.java
@@ -1,7 +1,14 @@
 package br.bolicos.api.dragons.application.repositories;
 
+import br.bolicos.api.dragons.domain.dragon.Dragon;
 import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
 
 @Repository
 public interface DragonRepository {
+    Dragon get(UUID id);
+    Dragon save(Dragon domain);
+    Dragon update(Dragon domain, UUID id);
+    UUID delete(UUID id);
 }
--- src/main/java/br/bolicos/api/dragons/infraestructure/http/controllers/DragonController.java
+++ src/main/java/br/bolicos/api/dragons/infraestructure/http/controllers/DragonController.java
@@ -1,8 +1,51 @@
 package br.bolicos.api.dragons.infraestructure.http.controllers;
 
+import br.bolicos.api.dragons.application.usecases.DragonService;
+import br.bolicos.api.dragons.infraestructure.http.dtos.DragonDTO;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
-@RestController("/dragons")
+import java.util.UUID;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/dragons")
 public class DragonController {
+    private DragonService dragonService;
 
+    @PostMapping
+    public Mono<DragonDTO> createDragon(@RequestBody DragonDTO dto) {
+        var dragon = dragonService.create(null);
+
+        return Mono.empty();
+    }
+
+    @GetMapping("/{id}")
+    public Mono<DragonDTO> getDragon(@PathVariable UUID id) {
+        var dragon = dragonService.get(id);
+
+        return Mono.empty();
+    }
+
+    @GetMapping
+    public Flux<DragonDTO> getAllDragons() {
+        var dragons = dragonService.getAll();
+
+        return Flux.empty();
+    }
+
+    @GetMapping("/byKingdom")
+    public Flux<DragonDTO> getDragonsByKingdom(@RequestParam UUID kingdomId) {
+        var dragons = dragonService.getByKingdom(kingdomId);
+
+        return Flux.empty();
+    }
 }
