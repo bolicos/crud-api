094c8215fa018cdb2a6fef07affe10ea1edef768
A. Bolico <51336956+bolicos@users.noreply.github.com>
2024-08-15 00:43:27 +0100

    enhancement: add an global http exception handler for many types of errors (#14)

--- src/main/java/br/bolicos/api/dragons/infraestructure/http/controllers/DragonController.java
+++ src/main/java/br/bolicos/api/dragons/infraestructure/http/controllers/DragonController.java
@@ -3,12 +3,16 @@ package br.bolicos.api.dragons.infraestructure.http.controllers;
 import br.bolicos.api.dragons.application.usecases.DragonService;
 import br.bolicos.api.dragons.infraestructure.http.dtos.DragonDTO;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -22,6 +26,7 @@ public class DragonController {
     private DragonService dragonService;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public Mono<DragonDTO> createDragon(@RequestBody DragonDTO dto) {
         var dragon = dragonService.create(null);
 
@@ -29,6 +34,7 @@ public class DragonController {
     }
 
     @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public Mono<DragonDTO> getDragon(@PathVariable UUID id) {
         var dragon = dragonService.get(id);
 
@@ -36,6 +42,7 @@ public class DragonController {
     }
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public Flux<DragonDTO> getAllDragons() {
         var dragons = dragonService.getAll();
 
@@ -43,9 +50,24 @@ public class DragonController {
     }
 
     @GetMapping("/byKingdom")
+    @ResponseStatus(HttpStatus.OK)
     public Flux<DragonDTO> getDragonsByKingdom(@RequestParam UUID kingdomId) {
         var dragons = dragonService.getByKingdom(kingdomId);
 
         return Flux.empty();
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteDragon(@PathVariable UUID id) {
+        dragonService.delete(id);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Mono<DragonDTO> createDragon(@PathVariable UUID id, @RequestBody DragonDTO dto) {
+        var dragon = dragonService.update(null, null);
+
+        return Mono.empty();
     }
 }
