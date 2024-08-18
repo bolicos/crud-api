c8762e62370f5b839562837a0a5617b92c452669
bolicos <apollobolico@gmail.com>
2024-08-18 19:49:23 -0300

    documentation: fix spring framework logo

--- src/main/java/br/bolicos/api/dragons/Application.java
+++ src/main/java/br/bolicos/api/dragons/Application.java
@@ -2,12 +2,10 @@ package br.bolicos.api.dragons;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
 @EnableWebFlux
 @SpringBootApplication
-@ConfigurationPropertiesScan(basePackages = "br.bolicos.api.dragons.setup")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
